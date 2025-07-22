2759d4589027021133b82b0bcdcdee2dc9f6a71e
unknown <angpao29@gmail.com>
2025-07-22 15:06:48 +0200

    Aggiustati test robula

--- mutanti_paper_ed_utilities/angular-example-no-id/selenium-tests/src/test/java/com/bbtutorials/users/RobulaXPathTest.java
+++ mutanti_paper_ed_utilities/angular-example-no-id/selenium-tests/src/test/java/com/bbtutorials/users/RobulaXPathTest.java
@@ -10,9 +10,9 @@ public class RobulaXPathTest extends BaseTest {
     driver.findElement(By.xpath("//label[contains(text(),'Nome:')]/input")).clear();
     driver.findElement(By.xpath("//label[contains(text(),'Nome:')]/input")).sendKeys("RobulaNome");
 
-    driver.findElement(By.xpath("//label[contains(text(),'Email:')]/input']")).click(); // Assumendo sia il secondo form-group
-    driver.findElement(By.xpath("//label[contains(text(),'Email:')]/input']]")).clear();
-    driver.findElement(By.xpath("//label[contains(text(),'Email:')]/input']")).sendKeys("robula@example.com");
+    driver.findElement(By.xpath("//label[contains(text(),'Email:')]/input")).click(); 
+    driver.findElement(By.xpath("//label[contains(text(),'Email:')]/input")).clear();
+    driver.findElement(By.xpath("//label[contains(text(),'Email:')]/input")).sendKeys("robula@example.com");
 
     driver.findElement(By.xpath("//button[text()='Invia Messaggio']")).click();
   }
