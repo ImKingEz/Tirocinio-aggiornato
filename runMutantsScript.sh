2fb53b763cdb5c240ebd09dfde8fc6fed26fbb3e
MyNameIsFred3 <volpealfredo12@gmail.com>
2025-09-20 10:43:28 +0200

    Rimosso script temporaneo

--- runMutantsScript.sh
+++ runMutantsScript.sh
@@ -64,8 +64,8 @@ if [ ! -f "$DEST_FILE" ]; then
     exit 1
 fi
 
-# LOCATOR_TYPES=("hook" "absolute" "relative" "robula" "selenium" "katalon")
-LOCATOR_TYPES=("relative")
+LOCATOR_TYPES=("hook" "absolute" "relative" "robula" "selenium" "katalon")
+# LOCATOR_TYPES=("relative")
 
 # Pulizia di esecuzioni precedenti
 echo "Pulizia di log e screenshot di esecuzioni precedenti..."
